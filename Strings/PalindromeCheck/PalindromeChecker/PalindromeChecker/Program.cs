2d94418727b67b64b5b58239e8ce44447cf2913c
schapaeva <schapaeva@seattletimes.com>
2016-05-07 16:54:11 -0700

    C# Palindrome Checker Solution

---
+++ Strings/PalindromeCheck/PalindromeChecker/PalindromeChecker/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome(Console.ReadLine()));
+            Console.ReadKey();
+        }
+
+        public static bool IsPalindrome(string input)
+        {
+            bool palindrome = true;
+            for (int i = 0; i < input.Length / 2 + 1; i++ )
+            {
+                if(input[i] != input[input.Length - i -1])
+                {
+                    palindrome = false;
+                    break;
+                }                
+            }
+            return palindrome;
+        }
+    }
+}
