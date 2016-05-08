d023cdb97e051d2410acb1f0bb61eb1a4ba57c99
schapaeva <schapaeva@seattletimes.com>
2016-05-07 21:23:47 -0700

    Adding another way to reverse the string

--- Strings/ReverseString/ReverseString/Program.cs
+++ Strings/ReverseString/ReverseString/Program.cs
@@ -11,13 +11,34 @@ namespace ReverseString
         static void Main(string[] args)
         {
             string input = Console.ReadLine();
+            Console.WriteLine(ReverseString1(input));
+            Console.WriteLine(ReverseString2(input));
+            
+            Console.ReadKey();
+        }
+
+        public static string ReverseString1(string input)
+        {
             string reverse = "";
-            for (int i = input.Length - 1; i > -1;  i--)
+            for (int i = input.Length - 1; i > -1; i--)
             {
                 reverse += input[i];
             }
-            Console.WriteLine(reverse);
-            Console.ReadKey();
+            return reverse;
         }
+
+        public static string ReverseString2(string input)
+        {
+            char[] chars = input.ToCharArray();
+            int end = input.Length -1;
+            for (int i = 0; i < end; i++, end--)
+            {
+                char tmp = chars[i];
+                chars[i] = chars[end];
+                chars[end] = tmp;
+            }
+            return new string(chars);
+        }
+
     }
 }
