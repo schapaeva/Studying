2305d3613c580f1a2bfc0df270cb834764d623ea
schapaeva <schapaeva@seattletimes.com>
2016-05-11 15:33:15 -0700

    JesseAndCookie solution + Sherlock and Array solution

---
+++ Queue/JesseAndCookies/JesseAndCookies/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JesseAndCookies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] temp = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(temp[0]);
+            int k = Convert.ToInt32(temp[1]);
+            int iterations = 0;
+
+            temp = Console.ReadLine().Split(' ');
+            int[] cookies = Array.ConvertAll(temp, Int32.Parse);
+            Queue<int> queue = new Queue<int>(cookies);
+            queue.OrderBy(q => q);
+
+            if (queue.Count > 0) //initial queue check
+            {
+                while (queue.Peek() < k)
+                {
+                    if (queue.Count > 0)
+                    {
+                        int currentCookie = queue.Dequeue();
+                        if (queue.Count > 0)
+                        {
+                            int nextCookie = queue.Dequeue();
+                            queue.Enqueue(1 * currentCookie + 2 * nextCookie);
+                            iterations++;
+                            queue.OrderBy(q => q);
+                        }
+                        else
+                            Console.WriteLine("-1");
+                    }
+                    else
+                        Console.WriteLine("-1");
+                }
+                Console.WriteLine(iterations.ToString());
+            }
+            else
+            {
+                Console.WriteLine("-1");
+            }
+
+
+        }
+
+    }
+}
---
+++ Search/SearchInArray/SearchInArray/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+                string[] tmp = Console.ReadLine().Split(' ');
+                long[] array = Array.ConvertAll(tmp, long.Parse);
+                CheckArray(array);
+            }
+        }
+
+        public static bool CheckArray(long[] array)
+        {
+            bool ifFoundIElement = false;
+            for (int i = 1; i < array.Length - 1; i++)
+            {
+                long sum1 = 0, sum2 = 0;
+                for (int left = 0; left < i; left++)
+                {
+                    sum1 += array[left];
+                }
+                for (int right = i + 1; right < array.Length; right++)
+                {
+                    sum2 += array[right];
+                }
+                if (sum1 == sum2)
+                {
+                    ifFoundIElement = true;
+                    Console.WriteLine("YES");
+                    break;
+                }
+            }
+            if (!ifFoundIElement)
+                Console.WriteLine("NO");
+            return ifFoundIElement;
+        }
+    }
+}
