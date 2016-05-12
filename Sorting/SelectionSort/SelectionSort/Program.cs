fb976da1f8c2bb08c29a010cdf3cc878d6c63d30
schapaeva <schapaeva@seattletimes.com>
2016-05-12 08:54:38 -0700

    Adding example of the selection sorting

---
+++ Sorting/SelectionSort/SelectionSort/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] temp = Console.ReadLine().Split(' ');
+            int[] array = Array.ConvertAll(temp, Int32.Parse);
+            SortIntArray(array);
+            PrintArray(array);
+            Console.ReadKey();
+        }
+
+        public static int FindMinPosition(int[] array, int start)
+        {
+            int minPosition = start;
+            for (int i = start; i < array.Length; i++)
+            {
+                if (array[i] < array[minPosition])
+                    minPosition = i;
+            }
+            return minPosition;
+        }
+
+        public static void SortIntArray(int[]array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int min = FindMinPosition(array, i);
+                if (min != i)
+                    Exchange(array, min, i);
+            }
+        }
+
+        public static void Exchange(int[] array, int index1, int index2)
+        {
+            int temp = array[index1];
+            array[index1] = array[index2];
+            array[index2] = temp;
+        }
+
+        public static void PrintArray(int[]array)
+        {
+            foreach (int val in array)
+                Console.Write(val.ToString() + " ");
+        }
+    }
+}
---
+++ Sorting/SelectionSort/SelectionSort/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SelectionSort")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Seattle Times")]
+[assembly: AssemblyProduct("SelectionSort")]
+[assembly: AssemblyCopyright("Copyright © Seattle Times 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ea8b80cf-6104-4bca-9c4f-9d030db323f8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
