74c0206c5065a2986f7d21381d7adb69f2e0fdb2
schapaeva <schapaeva@seattletimes.com>
2016-05-15 20:45:30 -0700

    Adding Employee example

---
+++ Trees/Tree/EmployeeTree/EmployeeTree/EmployeeTree.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeTreeExample
+{
+    public class Employee
+    {
+        private string name;
+        private double salary;
+        private List<Employee> subordinates;
+
+        public Employee(string name)
+        {
+            this.name = name;
+            this.salary = 0.0;
+            this.subordinates = null;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public List<Employee> Subordinates
+        {
+            get { return this.subordinates; }
+            set { this.subordinates = value; }
+        }
+
+        public void AddSubordinate(Employee employee)
+        {
+            if (this.subordinates == null)
+                this.subordinates = new List<Employee>();
+            this.subordinates.Add(employee);
+        }
+
+        public List<Employee> GetPath(string name)
+        {
+            var path = new Stack<Employee>();
+            path.Push(this);
+
+            var found = LookupPath(path, name);
+
+            if (found)
+            {
+                return path.Reverse().ToList();
+            }
+            else
+            {
+                throw new Exception("Could not find employee with name " + name);
+            }
+        }
+
+        private bool LookupPath(Stack<Employee> currentPath, string lookupName)
+        {
+            if (currentPath.Peek().Name == lookupName)
+            {
+                return true;
+            }
+
+            if (currentPath.Peek().Subordinates == null)
+            {
+                return false;
+            }
+
+            foreach (var child in currentPath.Peek().Subordinates)
+            {
+                currentPath.Push(child);
+
+                if (LookupPath(currentPath, lookupName))
+                {
+                    return true;
+                }
+
+                currentPath.Pop();
+            }
+            return false;
+        }
+    } 
+
+}
---
+++ Trees/Tree/EmployeeTree/EmployeeTree/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeTreeExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee frank = new Employee("Frank");
+            Employee tom = new Employee("Tom");
+            Employee rob = new Employee("Rob");
+            Employee bryan = new Employee("Bryan");
+            Employee sarah = new Employee("Sarah");
+            Employee sandra = new Employee("Sandra");
+            Employee marta = new Employee("Marta");
+            Employee evan = new Employee("Evan");
+            Employee chris = new Employee("Chris");
+
+            frank.AddSubordinate(tom);
+            frank.AddSubordinate(rob);
+            frank.AddSubordinate(bryan);
+            tom.AddSubordinate(sarah);
+            tom.AddSubordinate(sandra);
+            bryan.AddSubordinate(marta);
+            bryan.AddSubordinate(evan);
+            evan.AddSubordinate(chris);
+
+            PrintPathList(bryan.GetPath(chris.Name));
+
+            Console.ReadKey();
+        }
+
+        public static void PrintPathList(List<Employee> pathList)
+        {
+            for (int i = 0; i < pathList.Count; i++)
+            {
+                Console.Write(pathList[i].Name);
+                if (i != pathList.Count - 1)
+                    Console.Write("->");
+            }
+            Console.Write("\n");
+        }
+    }
+}
