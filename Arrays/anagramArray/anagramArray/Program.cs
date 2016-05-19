92028c3b0fce6c7a3c2db9e6c8c99cc7d05e35e2
schapaeva <schapaeva@seattletimes.com>
2016-05-19 16:29:46 -0700

    Interview Exercise

---
+++ Arrays/anagramArray/anagramArray/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anagramArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArray = Console.ReadLine().Split(' ');
+            string[] sortedArray = new string[inputArray.Length];
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                sortedArray[i] = SortWord(inputArray[i]);
+            }
+            Dictionary<string, List<string>> dict = new Dictionary<string,List<string>>();
+            for (int i = 0; i < sortedArray.Length; i++)
+            {
+                for (int j = i + 1; j < sortedArray.Length; j++)
+                {
+                    if (sortedArray[i] == sortedArray[j])
+                    {
+                        if (dict.ContainsKey(inputArray[i]))
+                        {
+                            dict[sortedArray[i]].Add(inputArray[j]);
+                        }                            
+                        else
+                        {
+                            dict.Add(inputArray[i], new List<string> { inputArray[j] });
+                        }
+                    }
+                }
+
+            }
+            List<List<string>> result = new List<List<string>>();
+            foreach(KeyValuePair<string, List<string>> pair in dict)
+            {
+                pair.Value.Add(pair.Key);
+                result.Add(pair.Value);
+            }
+            Console.Write("[");
+            foreach(var element in result)
+            {
+                Console.Write("[");
+                for (int i = 0; i < element.Count; i++)
+                {
+                    Console.Write(element[i]);
+                    if(i != element.Count -1)
+                        Console.Write(" ");
+                }
+                Console.Write("]");
+            }
+            Console.Write("]");
+            Console.ReadKey();
+        }
+
+        public static string SortWord(string word)
+        {
+            char[] wordChars = word.ToCharArray();
+            Array.Sort(wordChars);
+            return new string(wordChars);
+        }
+        public static void PrintNicely(List<List<string>> lisfOfLists)
+        {
+
+        }
+    }
+}
---
+++ Classes/Shapes/Shapes/Shapes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    abstract class Car
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public virtual void Draw()
+        {
+
+        }
+    }
+
+    class Circle : Car
+    {
+        public override void Draw()
+        {
+            base.Draw();
+        }
+    }
+}
