8fde19bef517d3131e65ec79a560b4d7f237f0d2
schapaeva <schapaeva@seattletimes.com>
2016-05-19 11:42:42 -0700

    CharactersFrequencies

---
+++ Arrays/CountAmount/CountAmount/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixSpiralPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] matrix = new int[n,n];
+            for (int i = 0; i < n; i++)
+            {
+                string[] temp = Console.ReadLine().Split(' ');
+                int[] line = Array.ConvertAll(temp, Int32.Parse);
+                if (line.Length != n)
+                    throw new Exception("Matrix should be size of " + n.ToString());
+                for (int j = 0; j < line.Length; j++)
+                {
+                    matrix[i,j] = line[j];
+                }
+            }
+            PrintSpiral(matrix);
+            PrintNormal(matrix);
+            Console.ReadKey();
+        }
+
+        public static void PrintSpiral(int[,] matrix)
+        {
+            Console.Write("\n\n");
+            int i, startRowInd = 0, endRowInd = matrix.GetLength(0), startColumnInd = 0, endColumnInd = matrix.GetLength(1);
+            //main loop
+            while (startRowInd < endRowInd && startColumnInd < endColumnInd)
+            {
+                //print first row
+                for (i = startColumnInd; i < endColumnInd; i++)
+                    Console.Write(matrix[startRowInd, i] + " ");
+                startRowInd++;
+                //print last column
+                for (i = startRowInd; i < endRowInd; i++)
+                    Console.Write(matrix[i, endColumnInd -1] + " ");
+                endColumnInd--;
+                //print last row
+                if (startRowInd < endRowInd)
+                {
+                    for (i = endColumnInd-1; i >= startColumnInd; i--)
+                        Console.Write(matrix[endRowInd -1, i] + " ");
+                    endRowInd--;
+                }
+                //print first column
+                if (startColumnInd < endColumnInd)
+                {
+                    for (i = endRowInd -1; i >= startRowInd; i--)
+                        Console.Write(matrix[i, startColumnInd] + " ");
+                    endColumnInd++;
+                }
+            }
+        }
+        public static void PrintNormal(int[,] matrix)
+        {
+            Console.Write("\n\n");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j].ToString() + " ");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
---
+++ Strings/CharactersFrequencies/CharactersFrequencies/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharactersFrequencies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            PrintDictionary(ParseString(input));
+            Console.ReadKey();
+        }
+
+        public static Dictionary<char, int> ParseString(string input)
+        {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (dict.ContainsKey(input[i]))
+                {
+                    dict[input[i]]++ ;
+                }
+                else
+                {
+                    dict.Add(input[i], 1);
+                }
+            }
+            return dict;
+        }
+
+        public static void PrintDictionary(Dictionary<char, int> dict)
+        {
+            foreach(KeyValuePair<char, int> pair in dict)
+            {
+                Console.Write("({0},{1})", pair.Key, pair.Value);
+            }
+        }
+    }
+}
