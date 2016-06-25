8e35fb2de0898bd7046b2c6704dac97a20637113
schapaeva <ksandra13@gmail.com>
2016-06-25 16:22:41 -0700

    Adding exercise about strings Overlaps

---
+++ Strings/IfStringOverlaps/IfStringOverlaps/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfStringOverlaps
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+            int t = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                string a = Console.ReadLine();
+                string b = Console.ReadLine();
+                if (IfSubstring(a, b))
+                    Console.WriteLine("YES");
+                else
+                    Console.WriteLine("NO");
+            }
+        }
+        public static bool IfSubstring(string a, string b)
+        {
+            HashSet<char> aHash = new HashSet<char>(a.ToCharArray());
+            HashSet<char> bHash = new HashSet<char>(b.ToCharArray());
+            if (aHash.Overlaps(bHash))
+                return true;
+            else
+                return false;
+        }
+    }
+}
