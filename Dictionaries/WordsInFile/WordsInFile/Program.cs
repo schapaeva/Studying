2b4fe926d7dba9071ea60b8bce01da6170a80908
schapaeva <schapaeva@seattletimes.com>
2016-08-01 16:47:14 -0700

    Example of counting words in file

---
+++ Dictionaries/WordsInFile/WordsInFile/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace WordsInFile
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"..\..\..\", @"TextFiles\")) + "article1.txt";
+
+            if (File.Exists(filePath))
+            {
+                string text = File.ReadAllText(filePath); 
+            }
+            else
+            {
+                Console.WriteLine("This file does not exist in this folder.");
+            }            
+
+        }
+
+        public static string[] NormalizeText(string text)
+        {
+            text.Trim();
+            string[] input = text.Split(' ');
+            string[] words = new string[input.Length]; 
+
+            for (int i = 0; i < input.Length; i ++)
+            {
+                words[i] = TrimPunctuation(input[i]);
+            }
+            return words;
+        }
+
+        public static string TrimPunctuation(string value)
+        {
+            int removeFromStart = 0;
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (char.IsPunctuation(value[i]))
+                {
+                    removeFromStart++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            // Count end punctuation.
+            int removeFromEnd = 0;
+            for (int i = value.Length - 1; i >= 0; i--)
+            {
+                if (char.IsPunctuation(value[i]))
+                {
+                    removeFromEnd++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            // No characters were punctuation.
+            if (removeFromStart == 0 &&
+                removeFromEnd == 0)
+            {
+                return value;
+            }
+            // All characters were punctuation.
+            if (removeFromStart == value.Length &&
+                removeFromEnd == value.Length)
+            {
+                return "";
+            }
+            // Substring.
+            return value.Substring(removeFromStart,
+                value.Length - removeFromEnd - removeFromStart);
+        }
+
+        public static Dictionary<string, int> CountWords(string[] words)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            foreach (string word in words)
+            {
+                if (!dict.ContainsKey(word))
+                {
+                    dict.Add(word, 1);
+                }
+                else
+                {
+                    dict[word]++;
+                }
+            }
+            return dict;
+        }
+
+    }
+}
