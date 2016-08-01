9c64d5fa123644ce43d6a86d9fe94b76fd566036
schapaeva <schapaeva@seattletimes.com>
2016-08-01 15:58:39 -0700

    Changing to symmetric method

--- Arrays/CommonAndDifferentNumbers/CommonAndDifferentNumbers/Program.cs
+++ Arrays/CommonAndDifferentNumbers/CommonAndDifferentNumbers/Program.cs
@@ -10,9 +10,13 @@ namespace CommonAndDifferentNumbers
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter first array of digits: ");
             int[] array1 = Array.ConvertAll(Console.ReadLine().Split(' '), Int32.Parse);
+            Console.Write("\nEnter second array of digits: ");
             int[] array2 = Array.ConvertAll(Console.ReadLine().Split(' '), Int32.Parse);
+            Console.Write("\nOverlaps: ");
             PrintArray(ArraysOverlap(array1, array2));
+            Console.Write("\nDifference: ");
             PrintArray(ArraysDifference(array1, array2));
             Console.ReadKey();
         }
@@ -29,13 +33,13 @@ namespace CommonAndDifferentNumbers
         {
             HashSet<int> hashset1 = new HashSet<int>(array1);
             HashSet<int> hashset2 = new HashSet<int>(array2);
-            hashset1.Except(array2);
+            hashset1.SymmetricExceptWith(array2);
             return hashset1.ToArray();
         }     
 
         public static void PrintArray(int[] array)
         {
-            Console.Write("\n");
+            //Console.Write("\n");
             foreach(int number in array)
             {
                  Console.Write(number.ToString() + " ");
