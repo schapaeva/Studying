960eff3ce9540633b63a1480caea3fcbc2999c71
schapaeva <ksandra13@gmail.com>
2016-08-01 23:03:17 -0700

    K-th element in Linked List

---
+++ LinkedList/Return_Kth_Element_Linked/Return_Kth_Element_Linked/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Return_Kth_Element_Linked
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter linked list elements separated by space: ");
+            string[] temp = Console.ReadLine().Split(' ');
+            LinkedList<string> linkedList = new LinkedList<string>(temp);
+            Console.Write("\nEnter k-th element from the END you want to show: ");
+            int k = Int32.Parse(Console.ReadLine());
+
+            if (k > 0 && k < linkedList.Count)
+            {
+                Console.WriteLine(Return_K(linkedList, k));
+            }
+            else
+            {
+                Console.WriteLine("Incorrect index.");
+            }
+            
+            Console.ReadKey();            
+        }
+
+        public static string Return_K(LinkedList<string> list, int k)
+        {
+            return list.ElementAt(list.Count - k);
+        }
+    }
+}
