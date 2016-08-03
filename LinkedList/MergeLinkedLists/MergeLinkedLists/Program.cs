ca0b60e960f987761fbad9490c099fdc6a18acd6
schapaeva <ksandra13@gmail.com>
2016-08-03 01:20:17 -0700

    Improving runtime

--- LinkedList/MergeLinkedLists/MergeLinkedLists/Program.cs
+++ LinkedList/MergeLinkedLists/MergeLinkedLists/Program.cs
@@ -10,35 +10,46 @@ namespace MergeLinkedLists
     {
         static void Main(string[] args)
         {
+            int iterations;
             int[] temp = Array.ConvertAll(Console.ReadLine().Split(' '), Int32.Parse);
             LinkedList<int> list1 = new LinkedList<int>(temp);
             temp = Array.ConvertAll(Console.ReadLine().Split(' '), Int32.Parse);
             LinkedList<int> list2 = new LinkedList<int>(temp);
-            PrintLinkedList(MergeLists(list1, list2));
+            PrintLinkedList(MergeLists(list1, list2, out iterations));
+            Console.WriteLine("\nIterations: " + iterations.ToString());
             Console.ReadKey();
         }
 
-        public static LinkedList<int> MergeLists(LinkedList<int> list1, LinkedList<int> list2)
+        public static LinkedList<int> MergeLists(LinkedList<int> list1, LinkedList<int> list2, out int iterations)
         {
+            iterations = 0;
             if (list1.First == null)
                 return list2;
             if (list2.First == null)
                 return list1;
 
             LinkedListNode<int> current2 = list2.First;
-            while(current2 != null)
-            {
-                LinkedListNode<int> current1 = list1.First;
-                while(current2.Value > current1.Value && current1 != list1.Last)
+            LinkedListNode<int> current1 = list1.First;
+            while (current2 != null)
+            {                
+                while (current2.Value > current1.Value && current1 != list1.Last)
                 {                           
                     current1 = current1.Next;
+                    iterations++;
                 }
 
                 if (current2.Value > current1.Value)
+                {
                     list1.AddAfter(current1, current2.Value);
+                    current1 = current1.Next;
+                }
                 else
+                {
                     list1.AddBefore(current1, current2.Value);
+                    current1 = current1.Previous;
+                }
                 current2 = current2.Next;
+                iterations++;
             }
             return list1;
         }
