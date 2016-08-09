f3b5219f799461e7a6fffab7329be0b4b8184e16
schapaeva <schapaeva@seattletimes.com>
2016-08-09 15:10:10 -0700

    Adding start of Merge Sort example

---
+++ Sorting/MergeSort/MergeSort/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Array.ConvertAll(Console.ReadLine().Split(' '), Int32.Parse);
+            PrintArray(MergeSort(array));
+            Console.ReadKey();
+        }
+
+        //Runtime: nLog2(n) + n
+        public static int[] MergeSort(int[] array)
+        {
+            if (array.Length <= 1)
+                return array;
+
+            int middleIndex = array.Length / 2;
+            //while ()
+            
+        }
+
+        public static int[] MergeSubroutine(int[] array1, int[] array2)
+        {
+
+        }
+
+        public static void PrintArray<T>(IEnumerable<T> array)
+        {
+            Console.Write("\nArray: ");
+            foreach(var obj in array)
+            {
+                Console.Write(obj.ToString() + " ");
+            }
+        }
+    }
+}
