dd1693486471cd067f9f35d0949d49335a9ae45d
schapaeva <schapaeva@seattletimes.com>
2016-08-10 12:03:14 -0700

    Merge Sort example

--- Sorting/MergeSort/MergeSort/Program.cs
+++ Sorting/MergeSort/MergeSort/Program.cs
@@ -11,23 +11,52 @@ namespace MergeSort
         static void Main(string[] args)
         {
             int[] array = Array.ConvertAll(Console.ReadLine().Split(' '), Int32.Parse);
-            PrintArray(MergeSort(array));
+            MergeSort_Recursive(array, 0, array.Length - 1);
+            PrintArray(array);
             Console.ReadKey();
         }
 
-        //Runtime: nLog2(n) + n
-        public static int[] MergeSort(int[] array)
+        //Runtime: nLog2(n)
+        public static void MergeSort_Recursive(int[] array, int left, int right)
         {
-            if (array.Length <= 1)
-                return array;
+            if (left < right && array.Length > 1)
+            {
+                int mid = (right + left) / 2;
+                MergeSort_Recursive(array, left, mid);
+                MergeSort_Recursive(array, mid + 1, right);
 
-            int middleIndex = array.Length / 2;
-            //while ()
-            
+                DoMerge(array, left, (mid + 1), right);
+            }            
         }
 
-        public static int[] MergeSubroutine(int[] array1, int[] array2)
+        public static void DoMerge(int[] array, int left, int mid, int right)
         {
+            int i, left_end, num_elements, tmp_pos;
+            int[] temp = new int[array.Length];
+
+            left_end = (mid - 1);
+            tmp_pos = left;
+            num_elements = right - left + 1;
+
+            while (left <= left_end && mid <= right)
+            {
+                if (array[left] <= array[mid])
+                    temp[tmp_pos++] = array[left++];
+                else
+                    temp[tmp_pos++] = array[mid++];
+            }
+
+            while (left <= left_end)
+                temp[tmp_pos++] = array[left++];
+
+            while (mid <= right)
+                temp[tmp_pos++] = array[mid++];
+
+            for (i = 0; i < num_elements; i++)
+            {
+                array[right] = temp[right];
+                right--;
+            }
 
         }
 
