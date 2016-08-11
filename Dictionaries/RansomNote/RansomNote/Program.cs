4a25a15b4fc2524115ef56a9858ff166a479ed94
schapaeva <schapaeva@seattletimes.com>
2016-08-11 13:29:46 -0700

    RansomNote example

---
+++ Dictionaries/RansomNote/RansomNote/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RansomNote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ransomNote = Console.ReadLine();
+            string magazine = Console.ReadLine();
+            Console.WriteLine(CanConstruct(ransomNote, magazine).ToString());
+            Console.ReadKey();
+        }
+
+        public static bool CanConstruct(string ransomNote, string magazine)
+        {
+            if (magazine.Length == 0 && ransomNote.Length == 0)
+                return true;
+            if (magazine.Length == 0)
+                return false;
+            if (ransomNote.Length == 0)
+                return true;
+
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            //Populate dictionary with letters from magazine
+            foreach (char ch in magazine)
+            {
+                if (!dict.ContainsKey(ch))
+                    dict.Add(ch, 1);
+                else
+                    dict[ch]++;
+            }
+
+            foreach (char ch in ransomNote)
+            {
+                if (!dict.ContainsKey(ch) || dict[ch] == 0)
+                    return false;
+                else
+                {
+                    dict[ch]--;
+                }
+            }
+            return true;
+        }
+    }
+}
