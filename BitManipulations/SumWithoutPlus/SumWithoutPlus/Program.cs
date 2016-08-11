5ce35a9a986c4dcef5d04765650f66dfa63a6407
schapaeva <schapaeva@seattletimes.com>
2016-08-11 13:10:09 -0700

    Sum of the integers without using +

---
+++ BitManipulations/SumWithoutPlus/SumWithoutPlus/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumWithoutPlus
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetSum(5, 6));
+            Console.ReadKey();
+        }
+
+        public static int GetSum(int a, int b)
+        {
+            while (b != 0)
+            {
+                int carry = (a & b); //CARRY is AND of two bits
+                a = a ^ b; //SUM of two bits is A XOR B
+                b = carry << 1; //shifts carry to 1 bit to calculate sum
+            }
+            return a;
+        }
+    }
+}
