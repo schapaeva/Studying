25d6bbda77e7fb0d9efc0678b0db8f172b067819
schapaeva <schapaeva@seattletimes.com>
2016-09-12 16:43:34 -0700

    Priority Queue using min Heap

---
+++ Queue/PriorityQueue/PriorityQueue/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriorityQueue
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // ...
+
+            // this priority queue uses "inverted" comparer, 
+            // so in fact it is max-priority queue
+            //PriorityQueue<int, string> q = new PriorityQueue<int, string>
+            //    (data, new MyComparer());
+
+            // ...
+        }
+    }
+
+    public class PriorityQueue<TPriority, TValue>
+    {
+        private List<KeyValuePair<TPriority, TValue>> _baseHeap;
+        private IComparer<TPriority> _comparer;
+
+        //Constructor
+        public PriorityQueue()
+            : this(Comparer<TPriority>.Default)
+        { 
+        }
+
+        public PriorityQueue(IComparer<TPriority> comparer)
+        {
+            if (comparer == null)
+                throw new ArgumentNullException();
+
+            _baseHeap = new List<KeyValuePair<TPriority, TValue>>();
+            _comparer = comparer;
+        }
+
+        public void Enqueue(TPriority priority, TValue value)
+        {
+            Insert(priority, value);
+        }
+
+        public void Insert(TPriority priority, TValue value)
+        {
+            KeyValuePair<TPriority, TValue> val = new KeyValuePair<TPriority, TValue>(priority, value);
+            _baseHeap.Add(val);
+
+            HeapifyFromEndToBeginning(_baseHeap.Count - 1);
+        }
+
+        public int HeapifyFromEndToBeginning(int pos)
+        {
+            if (pos >= _baseHeap.Count) return -1;
+
+            while(pos > 0)
+            {
+                int parentPos = (pos - 1) / 2;
+                if (_comparer.Compare(_baseHeap[parentPos].Key, _baseHeap[pos].Key) > 0)
+                {
+                    ExchangeElements(parentPos, pos);
+                    pos = parentPos;
+                }
+                else break;
+            }
+            return pos;
+        }
+
+        private void ExchangeElements(int pos1, int pos2)
+        {
+            KeyValuePair<TPriority, TValue> val = _baseHeap[pos1];
+            _baseHeap[pos1] = _baseHeap[pos2];
+            _baseHeap[pos2] = val;
+        }
+
+        public TValue DequeueValue()
+        {
+            return Dequeue().Value;
+        }
+
+        public KeyValuePair<TPriority, TValue> Dequeue()
+        {
+            if (!IsEmpty)
+            {
+                KeyValuePair<TPriority, TValue> result = _baseHeap[0];
+                DeleteRoot();
+                return result;
+            }
+            else
+                throw new InvalidOperationException("Priority queue is empty");
+        }
+
+        private void DeleteRoot()
+        {
+            if (_baseHeap.Count <= 1)
+            {
+                _baseHeap.Clear();
+                return;
+            }
+
+            _baseHeap[0] = _baseHeap[_baseHeap.Count - 1];
+            _baseHeap.RemoveAt(_baseHeap.Count - 1);
+
+            // heapify
+            HeapifyFromBeginningToEnd(0);
+        }
+
+        private void HeapifyFromBeginningToEnd(int pos)
+        {
+            if (pos >= _baseHeap.Count) return;
+
+            // heap[i] have children heap[2*i + 1] and heap[2*i + 2] and parent heap[(i-1)/ 2];
+
+            while (true)
+            {
+                // on each iteration exchange element with its smallest child
+                int smallest = pos;
+                int left = 2 * pos + 1;
+                int right = 2 * pos + 2;
+                if (left < _baseHeap.Count &&
+                    _comparer.Compare(_baseHeap[smallest].Key, _baseHeap[left].Key) > 0)
+                    smallest = left;
+                if (right < _baseHeap.Count &&
+                    _comparer.Compare(_baseHeap[smallest].Key, _baseHeap[right].Key) > 0)
+                    smallest = right;
+
+                if (smallest != pos)
+                {
+                    ExchangeElements(smallest, pos);
+                    pos = smallest;
+                }
+                else break;
+            }
+        }
+
+        public KeyValuePair<TPriority, TValue> Peek()
+        {
+            if (!IsEmpty)
+                return _baseHeap[0];
+            else
+                throw new InvalidOperationException("Priority queue is empty");
+        }
+
+        public TValue PeekValue()
+        {
+            return Peek().Value;
+        }
+
+        public bool IsEmpty
+        {
+            get { return _baseHeap.Count == 0; }
+        }
+    }
+
+    class MyComparer : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            // "inverted" comparison
+            // direct comparison of integers should return x - y
+            return y - x;
+        }
+    }
+}
