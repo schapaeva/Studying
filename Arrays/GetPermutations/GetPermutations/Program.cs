fbb63dc8a263391baaeb1b62c46ebe0721674f8e
schapaeva <ksandra13@gmail.com>
2016-09-26 18:16:53 -0700

    permutations

---
+++ Arrays/GetPermutations/GetPermutations/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetPermutations
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input String>");
+            string inputLine = Console.ReadLine();
+
+            Recursion rec = new Recursion();
+            rec.InputSet = rec.MakeCharArray(inputLine);
+            rec.CalcPermutation(0);
+
+            Console.Write("# of Permutations: " + rec.PermutationCount);
+            Console.ReadKey();
+        }
+    }
+    class Recursion
+    {
+        private int elementLevel = -1;
+        private int numberOfElements;
+        private int[] permutationValue = new int[0];
+
+        private char[] inputSet;
+        public char[] InputSet
+        {
+            get { return inputSet; }
+            set { inputSet = value; }
+        }
+
+        private int permutationCount = 0;
+        public int PermutationCount
+        {
+            get { return permutationCount; }
+            set { permutationCount = value; }
+        }
+
+        public char[] MakeCharArray(string InputString)
+        {
+            char[] charString = InputString.ToCharArray();
+            Array.Resize(ref permutationValue, charString.Length);
+            numberOfElements = charString.Length;
+            return charString;
+        }
+
+        public void CalcPermutation(int k)
+        {
+            elementLevel++;
+            permutationValue.SetValue(elementLevel, k);
+
+            if (elementLevel == numberOfElements)
+            {
+                OutputPermutation(permutationValue);
+            }
+            else
+            {
+                for (int i = 0; i < numberOfElements; i++)
+                {
+                    if (permutationValue[i] == 0)
+                    {
+                        CalcPermutation(i);
+                    }
+                }
+            }
+            elementLevel--;
+            permutationValue.SetValue(0, k);
+        }
+
+        private void OutputPermutation(int[] value)
+        {
+            foreach (int i in value)
+            {
+                Console.Write(inputSet.GetValue(i - 1));
+            }
+            Console.WriteLine();
+            PermutationCount++;
+        }
+    }
+}
---
+++ Classes/KeyboardAndMovieName/KeyboardAndMovieName/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyboardAndMovieName
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Keyboard
+    {
+        private int columns;
+        private Dictionary<char, Button> keys;
+
+        public 
+    }
+
+    public class Button
+    {
+        private char letter;
+        private int x;
+        private int y;
+        private bool available;
+
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public bool IfAvailable
+        {
+            get { return available == true; }
+            set { available = value; }
+        }
+
+        public char Letter
+        {
+            get { return letter; }
+            set { letter = value; }
+        }
+       
+        public Button(int _x, int _y, char _letter, bool _available)
+        {
+            x = _x;
+            y = _y;
+            letter = _letter;
+            available = _available;
+        }
+    }
+}
