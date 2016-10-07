deee0d354b5c004c30f6a588ee8a5539e462e8da
schapaeva <schapaeva@seattletimes.com>
2016-10-07 11:51:04 -0700

    Remove dups

---
+++ Arrays/RemoveDuplicates/RemoveDuplicates/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicates
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sArray = Console.ReadLine().Split(' ');
+            var sList = new List<string>();
+
+            foreach(string s in sArray)
+            {
+                if (!sList.Contains(s))
+                    sList.Add(s);
+            }
+
+            var sNew = sList.ToArray();
+            PrintArray(sNew);
+            Console.ReadKey();
+        }
+
+        public static void PrintArray(object[] array)
+        {
+            foreach (object obj in array)
+            {
+                Console.Write(obj.ToString() + " ");
+            }
+        }
+    }
+}
